e2d4cd45f2fd1422c357bff508017607f903d37d
Nik Fikri <nikfikrics@gmail.com>
2024-11-08 21:59:35 +0800

    Update urlPattern to support additional cases

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -12,9 +12,16 @@ struct RegexParser {
     static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_]*"
     static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_.]*"
     static let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
-        "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
-    "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
+    static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +  // Leading boundary
+        "((https?://|www\\.|pic\\.)" +  // Protocols and common prefixes
+        "[\\p{L}\\p{N}\\-_.]+" +  // Domain with Unicode support
+        "\\.[\\p{L}]{2,}" +  // TLD with Unicode support
+        "(\\.[\\p{L}]{2,})?" +  // Optional secondary TLD
+        "(:\\d{1,5})?" +  // Optional port
+        "(/[\\p{L}\\p{N}\\-./_?%&=;@]*)?" +  // Path with extended symbols
+        "(\\?[\\p{L}\\p{N}&=_%+-]*)?" +  // Optional query parameters
+        "(#[\\p{L}\\p{N}\\-_]*)?)" +  // Optional fragment
+        "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"  // Trailing boundary
 
     private static var cachedRegularExpressions: [String : NSRegularExpression] = [:]
 
